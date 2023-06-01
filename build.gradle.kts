2171e822f8622773d6439d7512d6d829f15bf0b5
tariqmetric <tariq@metricapp.co>
2023-06-02 00:27:21 +0500

    Ktor Test Server Practice

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "com.tariq"
+version = "1.0-SNAPSHOT"
+
+application{
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-core:2.3.0")
+    implementation("io.ktor:ktor-server-netty:2.3.0")
+    implementation("ch.qos.logback:logback-classic:1.2.5")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/com/tariq/Application.kt
@@ -0,0 +1,27 @@
+package com.tariq
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module(){
+    routing {
+        get("/"){
+            call.respondText("Hello World")
+        }
+    }
+}
+
+@Suppress("unused")
+fun Application.module2(){
+    routing {
+        get("/second"){
+            call.respondText("Hello from Second World")
+        }
+    }
+}
+
